02a3321263a42d8049b261ff1037367689499f99
DasVaca <dasvaca@DESKTOP-FEA57A6.localdomain>
2020-06-13 19:50:27 -0400

    Agregar proyecto a github

---
+++ HUD.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func show_game_over(msg):
+	$MessageLabel.text = msg
+	$MessageLabel.show()
+	start_menu()
+
+func start_menu():
+	yield(get_tree().create_timer(2), "timeout")
+	
+	$MessageLabel.text = "Bellaland"
+	$MessageLabel.show()
+	
+	yield(get_tree().create_timer(0.5), "timeout")
+	
+	$StartButton.show()
+
+func _on_StartButton_pressed():
+	emit_signal("start_game")
+	$MessageLabel.hide()
+	$StartButton.hide()
