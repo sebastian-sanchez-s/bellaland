dc4cb2f573aec3cdc121dd280d3079d483771d40
DasVaca <sebastian23082000@gmail.com>
2020-08-07 16:18:35 -0400

    Add getter and setters for external calls

--- src/code/Bella.gd
+++ src/code/Bella.gd
@@ -2,46 +2,70 @@ extends KinematicBody2D
 
 signal fall
 
-export var speed : int = 100
-export var jump_speed : int = 1000
-export var gravity : int = 500
+var walk_speed	: int = 250
+var jump_speed	: int = 500
+var gravity		: int = 800
 
-var screen_size
-var velocity : Vector2 = Vector2()
+var screen_size	: Vector2
+var velocity	: Vector2
 
-
-func _ready():
+func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-func set_position(pos):
-	position = pos
-
-func get_position():
-	return position
-
 func _physics_process(delta):
-	# Move left or right
+	# HORIZONTAL DYNAMIC
+	# movement
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.x *= speed
-	# Applying velocity
-	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x *= walk_speed
+	# animation
 	if is_on_floor():
-		if velocity.x == 0:
-			$AnimatedSprite.play("Idle")
-		else:
+		if velocity.x != 0:
 			$AnimatedSprite.play("Walk")
-	# Gravity
-	velocity.y += gravity * delta
-	# Jump
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y -= jump_speed
-		$AnimatedSprite.play("Jump")
-	# Bella looks right or left depending of input
+		else:
+			$AnimatedSprite.play("Idle")
+
+	
+	# VERTICAL DYNAMIC
+	if not is_on_floor():
+		velocity.y += gravity*delta
+	
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y -= jump_speed
+			$AnimatedSprite.play("Jump")
+	if Input.is_action_just_pressed("ui_down"):
+		if not is_on_floor():
+			velocity.y += jump_speed*0.5
+	
 	$AnimatedSprite.flip_h = velocity.x < 0
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	emit_signal("fall", "fall")
 
 func game_over():
-	velocity = Vector2.ZERO
+	set_velocity(Vector2.ZERO)
 	$AnimatedSprite.stop()
+
+# Getters and Setters are for external calls
+func get_walk_speed() -> int:
+	return walk_speed
+
+func get_jump_speed() -> int:
+	return jump_speed
+
+func get_gravity_force() -> int:
+	return gravity 
+
+func get_velocity() -> Vector2:
+	return velocity
+
+func set_velocity(velocity : Vector2) -> void:
+	self.velocity = velocity
+
+func set_position(position:Vector2) -> void:
+	self.position = position
+
+func get_position() -> Vector2:
+	return position
